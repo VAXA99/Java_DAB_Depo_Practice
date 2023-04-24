787730fd05bb14ba6fa193b29bde225f106f5ff6
VAXA99 <104323871+VAXA99@users.noreply.github.com>
2023-04-24 13:01:09 +0300

    Monday code review

--- src/main/resources/static/script.js
+++
@@ -1,9 +0,0 @@
-function showPopup() {
-    var popup = document.getElementById("myPopup");
-    popup.style.display = "block";
-}
-
-function hidePopup() {
-    var popup = document.getElementById("myPopup");
-    popup.style.display = "none";
-}
